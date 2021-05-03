5d3c01ac6be526891ea8071d59e0aee878ba08af
AlaistairP <alaistairp@gmail.com>
2021-05-03 10:43:26 +0100

    Add files via upload

---
+++ Discord-Music-Bot/config/bot.js
@@ -0,0 +1,17 @@
+module.exports = {
+    emojis: {
+        off: '❌',
+        error: '⚠️',
+        queue: '📊',
+        music: '🎶',
+        success: '✅',
+    },
+
+    discord: {
+        token: 'YOUR TOKEN HERE',
+        prefix: '-',
+        activity: 'Tunes',
+    },
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
